f868ec8931eaaa9ba03567b2c22c9bd579f7925a
Ivan22wyh <Ivan22wyh@outlook.com>
2024-05-17 20:30:27 +0800

    modify run_taggers.sh

--- run_taggers.sh
+++ run_taggers.sh
@@ -3,11 +3,7 @@
 
 dolma tag \
     --documents \
-	'400G_data/400G_data_dolma_formate/documents/RedPajamaWikipedia/*.gz' \
-        '400G_data/400G_data_dolma_formate/documents/RedPajamaArXiv/*.gz' \
-  	'400G_data/400G_data_dolma_formate/documents/RedPajamaBook/*.gz' \
-    	'400G_data/400G_data_dolma_formate/documents/RedPajamaC4/*.gz' \
-     	'400G_data/400G_data_dolma_formate/documents/RedPajamaCommonCrawl/*.gz' \
+	'output/*.gz' \
     --taggers  random_number_v1 \
         pii_regex_v2  \
 	cld2_en_paragraph_with_doc_score_v2 \
